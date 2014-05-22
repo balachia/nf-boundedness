9a9b3fce6277737abe21e895633ffb5bb504b677
Tony Vashevko <avashevko@gmail.com>
2014-05-22 11:44:27 -0700

    changes

--- rating-trajectories.R
+++ rating-trajectories.R
@@ -3,6 +3,8 @@ library(parallel)
 
 rm(list=ls())
 
+logit <- function(x) { log(x) - log(1-x) }
+
 setwd('~/Data/nf')
 
 valid.dt <- fread('valid_movies_2014.csv')
@@ -34,6 +36,8 @@ m.dt[, month_num := floor(month_id / 100) * 12 + month_id %% 100]
 m.dt[, mcount := month_num - min(month_num), by=movie_id]
 m.dt[, nmcount := month_num - max(month_num), by=movie_id]
 m.dt[, mfrac := mcount / (max(month_num) - min(month_num)), by=movie_id]
+m.dt[, pop_frac := rating_pop / all_pop]
+m.dt[, lpop_frac := logit(pop_frac)]
 setkey(m.dt, movie_id)
 
 ptm <- proc.time()
@@ -42,14 +46,38 @@ resdts <- mclapply(valid.seq, mc.cores=60, mc.preschedule=FALSE,
         mean.mc <- lm(rating_mean ~ mcount, data=m.dt[J(x)])
         mean.nmc <- lm(rating_mean ~ nmcount, data=m.dt[J(x)])
         mean.mf <- lm(rating_mean ~ mfrac, data=m.dt[J(x)])
+
+        popfrac.mc <- lm(pop_frac ~ mcount, data=m.dt[J(x)])
+        popfrac.nmc <- lm(pop_frac ~ nmcount, data=m.dt[J(x)])
+        popfrac.mf <- lm(pop_frac ~ mfrac, data=m.dt[J(x)])
+
+        lpopfrac.mc <- lm(lpop_frac ~ mcount, data=m.dt[J(x)])
+        lpopfrac.nmc <- lm(lpop_frac ~ nmcount, data=m.dt[J(x)])
+        lpopfrac.mf <- lm(lpop_frac ~ mfrac, data=m.dt[J(x)])
+
+
         cat(ec(x), '::', (proc.time() - ptm)[3], '\n')
         data.table(movie_id=x,
-                   mc.cons=mean.mc$coef[1],
-                   mc.b=mean.mc$coef[2],
-                   nmc.cons=mean.nmc$coef[1],
-                   nmc.b=mean.nmc$coef[2],
-                   mf.cons=mean.mf$coef[1],
-                   mf.b=mean.mf$coef[2]
+                   rmean_mc_cons=mean.mc$coef[1],
+                   rmean_mc_b=mean.mc$coef[2],
+                   rmean_nmc_cons=mean.nmc$coef[1],
+                   rmean_nmc_b=mean.nmc$coef[2],
+                   rmean_mf_cons=mean.mf$coef[1],
+                   rmean_mf_b=mean.mf$coef[2],
+                   
+                   popfrac_mc_cons=popfrac.mc$coef[1],
+                   popfrac_mc_b=popfrac.mc$coef[2],
+                   popfrac_nmc_cons=popfrac.nmc$coef[1],
+                   popfrac_nmc_b=popfrac.nmc$coef[2],
+                   popfrac_mf_cons=popfrac.mf$coef[1],
+                   popfrac_mf_b=popfrac.mf$coef[2],
+                   
+                   lpopfrac_mc_cons=lpopfrac.mc$coef[1],
+                   lpopfrac_mc_b=lpopfrac.mc$coef[2],
+                   lpopfrac_nmc_cons=lpopfrac.nmc$coef[1],
+                   lpopfrac_nmc_b=lpopfrac.nmc$coef[2],
+                   lpopfrac_mf_cons=lpopfrac.mf$coef[1],
+                   lpopfrac_mf_b=lpopfrac.mf$coef[2]
                    )
     })
 print(proc.time() - ptm)
